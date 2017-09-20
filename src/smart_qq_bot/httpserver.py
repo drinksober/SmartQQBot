8cc0f1ded2ed0e30f88325f353b590a57ad546fb
drinks <drinksober@foxmail.com>
2017-09-20 23:45:40 +0800

    add: add github webhook

--- src/smart_qq_bot/httpserver.py
+++ src/smart_qq_bot/httpserver.py
@@ -1,25 +1,27 @@
-from bottle import run, get, static_file, redirect
+from flask import Flask, redirect
+from webhook.github import Webhook
+
+app = Flask(__name__)  # Standard Flask app
+webhook = Webhook(app)  # Defines '/postreceive' endpoint
 
 
-@get('/')
+@app.route('/')
 def index():
     from smart_qq_bot.app import bot
     tpl = '<h1>If QR Code is shown, please scan!</h1>' \
           '<h2>Login Out Dated: {login_out_dated} </h2>' \
           '<img src="/qr_code.jpg" alt="qr_code"/>' \
           '<h1>click <a href="/re-login">here</a> to re-login</h1>'
-    return tpl.format(
-        login_out_dated=bot.login_out_dated
-    )
+    return tpl.format(login_out_dated=bot.login_out_dated)
 
 
-@get('/qr_code.jpg')
+@app.route('/qr_code.jpg')
 def qr_code():
     from smart_qq_bot.config import QR_CODE_FNAME
-    return static_file(QR_CODE_FNAME, root=".")
+    return app.send_static_file(QR_CODE_FNAME)
 
 
-@get('/re-login')
+@app.route('/re-login')
 def re_login():
     from smart_qq_bot.app import bot
     bot.login(no_gui=True)
@@ -27,4 +29,4 @@ def re_login():
 
 
 def run_server(host="0.0.0.0", port=8888):
-    run(host=host, port=port)
+    app.run(host=host, port=port)
