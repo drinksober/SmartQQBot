dab9f88e9257aeb78da3d13fee46497545a67e51
drinks <drinksober@foxmail.com>
2017-10-12 22:16:31 +0800

    format

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from webhook.github import Webhook
 
-
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app)  # Defines '/postreceive' endpoint
 
 
 def run(host="0.0.0.0", port=8888):
     app.run(host=host, port=port, debug=True)
+
 
 if __name__ == '__main__':
     run()
