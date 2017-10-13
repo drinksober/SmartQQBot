92ebda3fedb6b07e3b21413a3df9edabf6460f10
drinks <drinksober@foxmail.com>
2017-10-13 16:17:35 +0800

    add: add author

--- src/webhook/actions.py
+++ src/webhook/actions.py
@@ -7,11 +7,11 @@ webhook = Webhook()
 config = {
     'default_groups': [],
     'default_friends': [u'398869368'],
-    'hfa/adtracker': {'group': ['666188124']},
-    'hfa/adtracker-fe': {'group': ['666188124']},
-    'BronzeKing/ssrd': {'group': ['656991593']},
-    'BronzeKing/ssrd-fe': {'group': ['656991593']},
-    'BronzeKing/ssrd-prototye': {'group': ['656991593']},
+    'hfa/adtracker': {'groups': ['666188124']},
+    'hfa/adtracker-fe': {'groups': ['666188124']},
+    'BronzeKing/ssrd': {'groups': ['656991593']},
+    'BronzeKing/ssrd-fe': {'groups': ['656991593']},
+    'BronzeKing/ssrd-prototye': {'groups': ['656991593']},
 }
 default_groups = config['default_groups']
 default_friends = config['default_friends']
@@ -26,13 +26,12 @@ def on_push(data):
     if 'head_commit' in data:  # github
         full_name = data['repository']['full_name']
         commit = data['head_commit']
-        commit = commit['id'][-7:] + ': ' + commit['message']
-        text = '\n'.join((full_name, commit))
     else:
         full_name = data['project']['path_with_namespace']
         commit = data['commits'][0]
-        commit = commit['id'][-7:] + ': ' + commit['message']
-        text = '\n'.join((full_name, commit))
+    author = commit['author']['name']
+    commit = commit['id'][-7:] + ': ' + commit['message']
+    text = '\n'.join((author, full_name, commit))
     name = '/'.join(full_name.split('/')[1:])   # project name
     _config = config.get(full_name, {}) or config.get(name, {})
     for group in _config.get('groups', default_groups):
